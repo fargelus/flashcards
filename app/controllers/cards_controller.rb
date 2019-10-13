2109641b21172abe54bde13f31b51b6ed82b1fef
fargelus <ddraudred@gmail.com>
2019-10-13 08:26:29 +0300

    Codestyle

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,3 +1,5 @@
+# Controller for cards resources 
+# frozen_string_literal: true
 class CardsController < ApplicationController
   def index
     @cards = Card.all
--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,4 @@
+# CardsHelper - list of helpers for Cards controller
+# frozen_string_literal: true
 module CardsHelper
 end
