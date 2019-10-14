f9049cff137fa6c6482a08457a66d1634b5e43b8
fargelus <ddraudred@gmail.com>
2019-10-14 06:42:28 +0300

    Rubocop

--- db/migrate/20191012161459_create_cards.rb
+++ db/migrate/20191012161459_create_cards.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-      t.text :original_text, :null => false
-      t.text :translated_text, :null => false
-      t.date :review_date, :null => false
+      t.text :original_text, null: false
+      t.text :translated_text, null: false
+      t.date :review_date, null: false
 
       t.timestamps null: false
     end
