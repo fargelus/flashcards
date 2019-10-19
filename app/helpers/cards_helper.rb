9f81419fc7da1d8a00901c76e348dc9351e367a1
fargelus <ddraudred@gmail.com>
2019-10-19 14:56:13 +0300

    Add min/max for date

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -5,4 +5,12 @@ module CardsHelper
   def format_date(date)
     date.strftime('%d/%m/%Y')
   end
+
+  def today
+    DateTime.now.to_date.to_s
+  end
+
+  def days_after(n)
+    DateTime.now.next_day(n).to_date.to_s
+  end
 end
