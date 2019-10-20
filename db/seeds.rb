e449b356da4b83295aacf5446f48a8720153055d
fargelus <ddraudred@gmail.com>
2019-10-20 14:56:06 +0300

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,34 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'nokogiri'
+require 'open-uri'
+
+def fill_dict_from_page(dict)
+  url = 'http://1000mostcommonwords.com/1000-most-common-german-words/'
+  doc = Nokogiri::HTML(open(url))
+
+  selector = '.entry-content tr:nth-child(n+2) td:nth-child(n+2)'
+  words = doc.css(selector)
+
+  words.each_with_index do |link, i|
+    orig_text = link.content
+    tr_text = words[i + 1]
+
+    unless tr_text.nil?
+      dict_item = Hash.new
+      dict_item['original_text'] = orig_text
+      dict_item['translated_text'] = tr_text.content
+      dict.push(dict_item)
+    end
+  end
+
+  dict
+end
+
+def fill_db_data_from_dict(dict)
+  Card.import dict
+end
+
+dict = []
+fill_dict_from_page(dict)
+fill_db_data_from_dict(dict)
