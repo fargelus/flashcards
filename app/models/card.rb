7e0197d932d894fc7a62e4e2c7120cdef886ab37
fargelus <ddraudred@gmail.com>
2019-10-27 18:13:37 +0300

    Back link & random record

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,9 @@
 # frozen_string_literal: true
 
 class Card < ActiveRecord::Base
+  scope :need_review, -> { where('review_date <= ?', Date.today) }
+  scope :random_record, -> { order('RANDOM()').first }
+
   validates :original_text, :translated_text, :review_date, presence: true
   validates :original_text, uniqueness: true
 
