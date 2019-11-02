a446c350e87cea7346517e5a4f55562fe5ab8482
fargelus <ddraudred@gmail.com>
2019-11-02 10:57:33 +0300

    Home controller test

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  describe 'GET #home' do
+    it 'renders :index template' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+end
