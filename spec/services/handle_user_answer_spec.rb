4e32f249dc13927000150b61a4fee9f6ba21f82c
fargelus <ddraudred@gmail.com>
2019-11-07 21:25:37 +0300

    Some fixes

--- spec/services/handle_user_answer_spec.rb
+++ spec/services/handle_user_answer_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe HandleUserAnswer, type: :service do
   let (:answer) { FactoryBot.create(:answer, card_id: card.id) }
 
   describe '#translation_correct' do
-    it 'answer is not correct translation' do
+    it 'has wrong answer' do
       user_answer_handler = HandleUserAnswer.new(answer, card)
       check_result = user_answer_handler.translation_correct?
       expect(check_result).to be false
     end
 
-    it 'answer is correct translation' do
+    it 'has correct answer' do
       answer.answer = card.translated_text
       user_answer_handler = HandleUserAnswer.new(answer, card)
       check_result = user_answer_handler.translation_correct?
@@ -22,7 +22,7 @@ RSpec.describe HandleUserAnswer, type: :service do
   end
 
   describe '#start' do
-    it 'has review_date 3 days later' do
+    it 'has review_date 3 days later when answer correct' do
       answer.answer = card.translated_text
       HandleUserAnswer.new(answer, card).start
       expect(card.review_date).to eq Date.today.next_day(3)
