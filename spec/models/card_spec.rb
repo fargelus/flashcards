e11af307961c402133195b8b65181038963d49c5
fargelus <ddraudred@gmail.com>
2019-11-21 12:24:43 +0300

    Tests for model

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Card, type: :model do
 
   describe 'associations' do
     it { should have_many(:answers) }
+    it { should belong_to(:user) }
   end
 
   context 'check attributes' do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:user) { create(:user) }
+
+  describe 'associations' do
+    it { should have_many(:cards) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+    it { should validate_uniqueness_of :email }
+  end
 end
