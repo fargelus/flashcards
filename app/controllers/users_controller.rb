d4dc0c67ebde04fe8bc3e659a38a8e6feedca15a
fargelus <ddraudred@gmail.com>
2019-11-22 17:29:00 +0300

    Password confirmation when signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,24 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  # GET /users/new
   def new
     @user = User.new
   end
 
   def show; end
 
-  # GET /users/1/edit
   def edit; end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to root_path, notice: I18n.t(:reg_success)
+    else
+      render 'new'
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -39,5 +29,15 @@ class UsersController < ApplicationController
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :crypted_password,
+      :crypted_password_confirmation
+    )
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,11 @@ class User < ApplicationRecord
   has_many :cards, dependent: :destroy
 
   validates :email, :crypted_password, presence: true
+
   validates :email, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   validates :crypted_password, length: { maximum: 20 }
+  validates_presence_of :crypted_password_confirmation, if: :crypted_password_changed?
+  validates_confirmation_of :crypted_password
 end
