4c3f434c02fa6f55e707e69cfe5a5923a243347d
fargelus <ddraudred@gmail.com>
2019-11-22 12:42:00 +0300

    Left only remember me sorcery module

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_191_122_081_622) do
+ActiveRecord::Schema.define(version: 20_191_122_093_006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
