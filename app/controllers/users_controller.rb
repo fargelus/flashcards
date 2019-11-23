4d37cac64bafc4c3929730ddcf817d92ec48f1fc
fargelus <ddraudred@gmail.com>
2019-11-23 17:06:23 +0300

    Edit user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
+# Controller for card's users
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :fetch_user, only: %i[edit update]
+
   def new
     @user = User.new
   end
-
-  def show; end
 
   def edit; end
 
@@ -20,18 +21,18 @@ class UsersController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to root_path, notice: I18n.t(:account_updated)
+    else
+      render 'edit'
     end
   end
 
   private
+
+  def fetch_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(
