6a3ec96f22bf9c6eeb63b845a5dee67cef33fac3
fargelus <ddraudred@gmail.com>
2019-12-01 10:29:21 +0300

    Decks CRUD

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class DecksController < ApplicationController
+  before_action :fetch_deck, only: %i[edit update destroy]
   helper_method :decks?
 
   def index
@@ -15,14 +16,42 @@ class DecksController < ApplicationController
     @deck = Deck.new
   end
 
+  def edit; end
+
+  def update
+    if @deck.update(deck_params)
+      redirect_to decks_path, notice: I18n.t(:deck_updated)
+    else
+      render 'edit'
+    end
+  end
+
   def create
-    name = params.require(:deck).permit(:name)
-    @deck = Deck.new(name)
+    @deck = Deck.new(deck_params)
 
     if @deck.save
       redirect_to decks_path, notice: I18n.t(:deck_created)
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @deck.destroy
+    redirect_to decks_path
+  end
+
+  private
+
+  def fetch_deck
+    @deck = Deck.find(params[:id])
+  end
+
+  def deck_params
+    params.require(:deck).permit(
+      :name,
+      :description,
+      :activity
+    )
   end
 end
--- db/migrate/20191201053834_create_decks.rb
+++ db/migrate/20191201053834_create_decks.rb
@@ -5,7 +5,7 @@ class CreateDecks < ActiveRecord::Migration[5.0]
     create_table :decks do |t|
       t.string :name
       t.text :description
-      t.boolean :activity, :default => false
+      t.boolean :activity, default: false
 
       t.timestamps
     end
