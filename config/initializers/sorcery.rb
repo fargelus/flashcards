93e29d340c031fe1153ceb1318ffb0fd44fdd9cf
fargelus <ddraudred@gmail.com>
2019-12-13 11:48:45 +0300

    Get github redirect uri from env

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -132,7 +132,7 @@ Rails.application.config.sorcery.configure do |config|
 
   config.github.key = ENV['GITHUB_KEY']
   config.github.secret = ENV['GITHUB_SECRET']
-  config.github.callback_url = 'http://0.0.0.0:3000/oauth/callback?provider=github'
+  config.github.callback_url = ENV['GITHUB_CALLBACK']
   config.github.user_info_mapping = { email: 'name' }
   config.github.scope = ''
 
