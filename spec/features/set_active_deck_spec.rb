36039bbac5bef180d937c8b62cda4259b2cbd0ce
fargelus <ddraudred@gmail.com>
2019-12-27 16:34:41 +0300

    Fix spec

--- spec/features/set_active_deck_spec.rb
+++ spec/features/set_active_deck_spec.rb
@@ -4,14 +4,13 @@ require 'rails_helper'
 
 RSpec.describe 'Set active deck', type: :feature do
   let!(:user) { create(:user) }
-  let!(:user_pwd) { attributes_for(:user)[:password] }
   let!(:deck) { create(:deck, user: user) }
   let(:locale) { create(:locale, user: user).locale }
 
   describe 'deck activation' do
     before do
       login_user(user, user_sessions_url)
-      visit edit_deck_path(locale, deck)
+      visit edit_deck_path(deck)
       find(:css, '#deck_activity').set(true)
       click_button 'commit'
     end
@@ -22,7 +21,7 @@ RSpec.describe 'Set active deck', type: :feature do
 
     it 'renders link to current deck cards in menu' do
       looking_name = deck.name.capitalize
-      expect(page).to have_link looking_name, href: deck_cards_path(locale, deck)
+      expect(page).to have_link looking_name, href: deck_cards_path(deck)
     end
   end
 end
