bf5d56dc28f9abd9fc01779e0473018c1a96b342
fargelus <ddraudred@gmail.com>
2020-01-05 20:24:04 +0300

    Clean

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,16 +18,17 @@ class HomeController < ApplicationController
   def access_allowed
     define_next_card
     @answer = PrevUserAnswerService.call(@card) if @card.present?
-    response_js if session[:guess_card_id]
-    session[:guess_card_id] = nil
+    make_response
   end
 
   def define_next_card
     @card = GetViewedCardService.call(current_user, session[:guess_card_id])
+    session[:guess_card_id] = nil
   end
 
-  def response_js
+  def make_response
     respond_to do |format|
+      format.html
       format.js
     end
   end
