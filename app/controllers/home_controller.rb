9b76e54d9ad8598e2fb703f24773888a919e5154
fargelus <ddraudred@gmail.com>
2020-01-06 11:02:06 +0300

    Notice to js

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -29,7 +29,7 @@ class HomeController < ApplicationController
   def make_response
     respond_to do |format|
       format.html
-      format.js
+      format.js { flash = flash }
     end
   end
 end
